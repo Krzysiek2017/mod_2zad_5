6e129944aefdb6bf42da033c746986231d492c5d
Krzysztof <musashimiyamoto@interia.eu>
2018-01-17 12:48:57 +0100

    mod_2zad_5

---
+++ components/Contacts.js
@@ -0,0 +1,22 @@
+﻿var Contacts = React.createClass({
+    propTypes: {
+        items: React.PropTypes.array.isRequired,
+    },
+
+    render: function() {
+        var contacts = this.props.items.map(function(contact) {
+            return (
+            <Contact item={contact} key={contact.id}>
+            )
+        });
+
+        return (
+            <ul className={contactList}>contacts</ul>
+            
+            
+            React.createElement('ul', {
+                className: 'contactList'
+            }, contacts)
+        );
+    }
+});
