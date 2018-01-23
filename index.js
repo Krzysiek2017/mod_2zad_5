0a6de64beb46aa56d14d6b657d0054a9656f3fc1
Krzysztof <musashimiyamoto@interia.eu>
2018-01-23 22:16:00 +0100

    poprawki

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var fs = require('fs');
-var http = require('http');
+var fs = require("fs");
+var http = require("http");
 
 var server = http.createServer();
-server.on('request', function(request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf8");
-    if (request.method === 'GET' && request.url === '/') {
-        fs.readFile('./index.html', (err, data) => {
-            if (err) throw err;
-            response.write(data);
-            response.end();
-        })
-    } else {
-        response.setHeader("Content-Type", "image/jpg; charset=utf8");
-        fs.readFile('./images/picture.jpg', (err, data) => {
-            if (err) throw err;
-            response.write(data);
-            response.end();
-        })
-    }
+server.on("request", function(request, response) {
+  response.setHeader("Content-Type", "text/html; charset=utf8");
+  if (request.method === "GET" && request.url === "/") {
+    fs.readFile("./index.html", (err, data) => {
+      if (err) throw err;
+      response.write(data);
+      response.end();
+    });
+  } else {
+    response.setHeader("Content-Type", "image/jpg; charset=utf8");
+    fs.readFile("./images/picture.jpg", (err, data) => {
+      if (err) throw err;
+      response.write(data);
+      response.end();
+    });
+  }
 });
 server.listen(8080);
